a396e8988dc385879756bd537fe759418d62f324
Zackary Ramirez <zackaryramirez96@tamu.edu>
2020-05-29 22:37:38 -0500

    basic routing for home page as well as component

--- personal-site/src/app/app.module.ts
+++ personal-site/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  // {path: '', component: HomeComponent},
+  // {path: '', component: HomeComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
