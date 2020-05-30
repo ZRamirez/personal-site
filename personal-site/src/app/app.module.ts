19a6f863461c45da5a8348d92423eac52fc618dc
Zackary Ramirez <zackaryramirez96@tamu.edu>
2020-05-29 22:56:52 -0500

    added basic routing links for each page.

--- personal-site/src/app/app.module.ts
+++ personal-site/src/app/app.module.ts
@@ -5,18 +5,27 @@ import {Routes, RouterModule } from '@angular/router'
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { SpotifyComponent } from './spotify/spotify.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
-  // {path: '', component: HomeComponent},
-  // {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'spotify', component: SpotifyComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'contact', component: ContactComponent},
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    SpotifyComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
