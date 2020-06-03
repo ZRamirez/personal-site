ce9b8fb2619494ad68c5f243c0581c222f109682
Zackary Ramirez <zackaryramirez@zackarys-mbp.attlocal.net>
2020-06-02 19:06:45 -0500

    finished landing page aesthetic

--- personal-site/src/app/home/home.component.ts
+++ personal-site/src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { Router, NavigationEnd } from '@angular/router';
 
+declare var VANTA;
+
 
 @Component({
   selector: 'app-home',
@@ -13,12 +15,21 @@ export class HomeComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit(): void {
-    this.router.events.subscribe((evt) => {
-      if (!(evt instanceof NavigationEnd)) {
-          return;
-      }
-      window.scrollTo(0, 0)
-  });
+
+    //for the landing page animations
+    VANTA.NET({
+      el: "#landingPage",
+      mouseControls: true,
+      touchControls: true,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      color: 0xd0f25,
+      backgroundColor: 0xae95dc,
+      points: 9.00,
+      maxDistance: 22.00
+    })
   }
 
   onClickScrollToHome(el: HTMLElement) {
