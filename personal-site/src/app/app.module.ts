15fc36995c2686e18655ef98e869f0751cea2067
Zackary Ramirez <zackaryramirez96@tamu.edu>
2020-08-18 17:04:18 -0500

    all pages are done

--- personal-site/src/app/app.module.ts
+++ personal-site/src/app/app.module.ts
@@ -22,7 +22,7 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     BrowserModule,
     RouterModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
